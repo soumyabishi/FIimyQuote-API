3e5deb5cf10d33c156700f63a93d72202a811725
Soumyabishi <soumyabishi@outlook.com>
2018-03-06 20:18:55 +0530

    Allowed hosts error :shit:

--- backend/settingsdev.py
+++ backend/settingsdev.py
@@ -6,3 +6,4 @@ ALLOWED_HOSTS += INTERNAL_IPS
 ALLOWED_HOSTS.append('localhost')
 INSTALLED_APPS.append('debug_toolbar')
 MIDDLEWARE.append('debug_toolbar.middleware.DebugToolbarMiddleware')
+ALLOWED_HOSTS = ['*']
